f82cebb6b7276e574e32bc2baa644cee5b649119
hhuucckk588 <76555779+hhuucckk588@users.noreply.github.com>
2021-11-13 19:26:53 +0200

    Update Login.js

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -1,16 +1,8 @@
-var url_string = window.location.href;
-var url = new URL(url_string);
-var code = url.searchParams.get("code");
-
-document.getElementById('code_display').innerHTML = code;
-
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://discord.com/api/v8/oauth2/token", true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-xhr.send(JSON.stringify({
-        'client_id': 881830919680454676,
-        'client_secret': 'CmeqYgSeQlgZLYwj95hf4hQmbNWL4wbf',
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': 'http://localhost:8000/after_login.html'
-}));
+const code=new URL(window.location.href).searchParams.get('code');
+document.getElementById('code_display').innerHTML=code;
+const options={method:'POST',                                              headers: {
+                                                  'Code': code
+                                              }}
+fetch('https://a566-2a00-a040-199-845d-282f-98ad-2b9d-5e5e.ngrok.io/test',options)
+    .then(respond=>respond.text)
+    .then(data=>{document.getElementById('code_display').innerHTML='k';}).catch(error=>{console.error('Error:',error);});
